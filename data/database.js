39911b405618e2f687d113e7bf3db36350a2c23f
BalamuruganD-Hexaware <balamurugand@hexaware.com>
2022-10-30 17:32:06 +0530

    initial commit

---
+++ data/database.js
@@ -0,0 +1,25 @@
+import { MongoClient } from 'mongodb';
+
+const clusterAddress = process.env.DB_CLUSTER_ADDRESS;
+const dbUser = process.env.DB_USERNAME
+const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+
+const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+
+console.log('Trying to connect to db');
+
+try {
+  await client.connect();
+  await client.db(dbName).command({ ping: 1 });
+  console.log('Connected successfully to server');
+} catch (error) {
+  console.log('Connection failed.');
+  await client.close();
+  console.log('Connection closed.');
+}
+
+const database = client.db(dbName);
+
+export default database;
